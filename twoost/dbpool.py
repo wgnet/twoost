19d737cdc3d39a248825a805a228b38e9386a5bf
Andrei Zhlobich <a_zhlobich@wargaming.net>
2015-01-05 15:41:20 +0300

    DB pools - logging & tests

--- twoost/dbpool.py
+++ twoost/dbpool.py
@@ -16,20 +16,31 @@ __all__ = ['DatabaseService', 'make_dbpool']
 
 class TwoostConnectionPool(ConnectionPool):
 
+    reconnect = True
+    init_conn = None
+
     def __init__(self, *args, **kwargs):
-        ConnectionPool.__init__(self, **kwargs)
-        self.cp_init_conn = kwargs.pop('cp_init_conn')
+        ConnectionPool.__init__(self, *args, **kwargs)
+        self.cp_init_conn = kwargs.pop('cp_init_conn', None)
+        self._database = kwargs.get('database') or kwargs.get('db')
         if isinstance(self.cp_init_conn, basestring):
             self.cp_init_conn = reflect.namedAny(self.cp_init_conn)
 
     def _disconnect_current(self):
         conn = self.connections.get(self.threadID())
         if conn:
-            logger.debug("disconnect %r", conn)
+            logger.debug("disconnect %r from %s", conn, self._database)
             self.disconnect(conn)
 
     def retry_on_error(self, e):
         return False
+
+    def _mk_log(fn):
+        @functools.wraps(fn)
+        def wrapper(self, *args, **kwargs):
+            logger.debug("db %s - %s(*%r, **%r)", self._database, fn.__name__, args, kwargs)
+            return fn(self, *args, **kwargs)
+        return wrapper
 
     def _mk_retry(fn):
 
@@ -37,7 +48,7 @@ class TwoostConnectionPool(ConnectionPool):
         def wrapper(self, *args, **kwargs):
             try:
                 return fn(self, *args, **kwargs)
-            except self.retry_on_errors as e:
+            except Exception as e:
                 if not self.retry_on_error(e):
                     raise
                 # connection lost etc.
@@ -47,9 +58,10 @@ class TwoostConnectionPool(ConnectionPool):
 
         return wrapper
 
-    # not wrap runInteraction & runWithConnection
-    runQuery = _mk_retry(ConnectionPool.runQuery)
-    runOperation = _mk_retry(ConnectionPool.runOperation)
+    runQuery = _mk_log(_mk_retry(ConnectionPool.runQuery))
+    runOperation = _mk_log(_mk_retry(ConnectionPool.runOperation))
+    runInteraction = _mk_log(ConnectionPool.runInteraction)
+    runWithConnection = _mk_log(ConnectionPool.runWithConnection)
 
     def prepare_connection(self, connection):
         if self.cp_init_conn:
@@ -67,10 +79,14 @@ class TwoostConnectionPool(ConnectionPool):
 class PGSqlConnectionPool(TwoostConnectionPool):
 
     def __init__(self, *args, **kwargs):
+
+        self.init_hstore = kwargs.pop('init_hstore', None)
+
         import psycopg2
         import psycopg2.extras
-        ConnectionPool.__init__(self, *args, cursor_factory=psycopg2.extras.DictCursor, **kwargs)
-        self.pg_extras = kwargs.pop('pg_extras', ['hstore', 'json'])
+        TwoostConnectionPool.__init__(
+            self, *args, cursor_factory=psycopg2.extras.DictCursor, **kwargs)
+
         self._retry_on_errors = (psycopg2.OperationalError, psycopg2.InterfaceError)
 
     def retry_on_error(self, e):
@@ -78,24 +94,37 @@ class PGSqlConnectionPool(TwoostConnectionPool):
 
     def prepare_connection(self, connection):
         import psycopg2.extras
-        for e in self.pg_extras:
-            getattr(psycopg2.extras, "register_" + e)(connection)
-        return TwoostConnectionPool.prepare_connection(connection)
+
+        if self.init_hstore:
+            psycopg2.extras.register_hstore(connection)
+
+        elif self.init_hstore is None:
+            try:
+                psycopg2.extras.register_hstore(connection)
+            except psycopg2.ProgrammingError as e:
+                if "hstore.sql" not in str(e):
+                    raise
+                logger.debug("hstore type not found in database")
+
+        return TwoostConnectionPool.prepare_connection(self, connection)
 
 
 class MySQLConnectionPool(TwoostConnectionPool):
 
     def __init__(self, *args, **kwargs):
+
         import MySQLdb
         import MySQLdb.cursors
-        ConnectionPool.__init__(self, *args, cursorclass=MySQLdb.cursors.DictCursor, **kwargs)
-        self.retry_on_errors = (MySQLdb.OperationalError,)
+        TwoostConnectionPool.__init__(
+            self, *args, cursorclass=MySQLdb.cursors.DictCursor, **kwargs)
+
+        self._retry_on_errors = (MySQLdb.OperationalError,)
 
     def retry_on_error(self, e):
         return isinstance(e, self._retry_on_errors) and e[0] in (2006, 2013)
 
 
-class _SQLiteConnectionPool(TwoostConnectionPool):
+class SQLiteConnectionPool(TwoostConnectionPool):
 
     def prepare_connection(self, connection):
         import sqlite3
@@ -113,7 +142,7 @@ def make_sqlite_dbpool(db):
     db['database'] = os.path.expandvars(db['database'])
 
     logger.debug("connecting to sqlite db %r", db['database'])
-    return _SQLiteConnectionPool(
+    return SQLiteConnectionPool(
         'sqlite3',
         detect_types=sqlite3.PARSE_DECLTYPES,
         check_same_thread=False,
@@ -124,14 +153,18 @@ def make_sqlite_dbpool(db):
 def make_mysql_dbpool(db):
 
     db = dict(db)
-    db['db'] = db.pop('database')
-    db['passwd'] = db.pop('password')
+
+    if 'db' not in db and 'database' in db:
+        db['db'] = db.pop('database')
+    if 'passwd' not in db and 'password' in db:
+        db['passwd'] = db.pop('password')
+
     db.setdefault('cp_reconnect', True)
     db.setdefault('charset', 'utf8')
     db.setdefault('host', 'localhost')
     db.setdefault('port', 3306)
 
-    logger.debug("connecting to mysqldb %r", db['db'])
+    logger.debug("connecting to mysqldb %r", db.get('db'))
     return MySQLConnectionPool('MySQLdb', **db)
 
 
@@ -142,7 +175,7 @@ def make_pgsql_dbpool(db):
     db.setdefault('host', 'localhost')
     db.setdefault('port', 5432)
 
-    logger.debug("connecting to pgsql %r", db['database'])
+    logger.debug("connecting to pgsql %r", db.get('database'))
     return PGSqlConnectionPool('psycopg2', **db)
 
 
---
+++ twoost/tests/test_dbpool.py
@@ -0,0 +1,91 @@
+# coding: utf-8
+
+from __future__ import print_function, division, absolute_import
+
+import os
+
+from twisted.internet import defer
+from twisted.trial.unittest import TestCase
+
+from twoost import dbpool
+from twoost._misc import mkdir_p
+
+
+class SqlitePoolTest(TestCase):
+
+    def setUp(self):
+        td = os.environ['TEST_TMP_DIR'] = self.mktemp()
+        mkdir_p(td)
+
+    @defer.inlineCallbacks
+    def test_sqlite3_driver(self):
+
+        db = dbpool.make_dbpool({
+            'driver': 'sqlite',
+            'database': "$TEST_TMP_DIR/test.db",
+        })
+
+        yield db.runOperation("CREATE TABLE twoost_the_table (x, y)")
+        yield db.runOperation("INSERT INTO twoost_the_table (x, y) VALUES (?, ?)", [1, 2])
+        yield db.runOperation("INSERT INTO twoost_the_table (x, y) VALUES (?, ?)", [10, 20])
+        rows = yield db.runQuery("SELECT x FROM twoost_the_table")
+        self.assertEquals([{'x': 1}, {'x': 10}], map(dict, rows))
+
+        def delete_rows(txn):
+            txn.execute("DELETE FROM twoost_the_table")
+
+        yield db.runInteraction(delete_rows)
+        rows = yield db.runQuery("SELECT x FROM twoost_the_table")
+        self.assertEquals([], rows)
+
+        yield db.runOperation("DROP TABLE twoost_the_table")
+
+        db.close()
+
+    def test_db_service(self):
+
+        dbs = dbpool.DatabaseService({
+            'db1': {'driver': 'sqlite', 'database': "$TEST_TMP_DIR/dbs_1.db"},
+            'db2': {'driver': 'sqlite', 'database': "$TEST_TMP_DIR/dbs_2.db"},
+            'db3': {'driver': 'sqlite', 'database': "$TEST_TMP_DIR/dbs_3.db"},
+        })
+        dbs.startService()
+
+        self.assertEquals(3, len(dbs.db_pools))
+        self.assertIsInstance(dbs['db1'], dbpool.SQLiteConnectionPool)
+        self.assertIsInstance(dbs['db2'], dbpool.SQLiteConnectionPool)
+        self.assertIsInstance(dbs['db3'], dbpool.SQLiteConnectionPool)
+
+        dbs.stopService()
+
+
+class PGDbPoolTest(TestCase):
+
+    @defer.inlineCallbacks
+    def test_pgsql_driver(self):
+
+        db = dbpool.make_dbpool({
+            'driver': 'pgsql',
+            'database': 'test',
+            'user': 'test',
+            'password': 'test',
+        })
+
+        yield db.runOperation("DROP TABLE IF EXISTS twoost_the_table")
+        yield db.runOperation("CREATE TABLE twoost_the_table (x int, y int)")
+
+        yield db.runOperation("INSERT INTO twoost_the_table (x, y) VALUES (%s, %s)", [1, 2])
+        yield db.runOperation("INSERT INTO twoost_the_table (x, y) VALUES (%s, %s)", [10, 20])
+
+        rows = yield db.runQuery("SELECT x FROM twoost_the_table")
+        self.assertEquals([{'x': 1}, {'x': 10}], map(dict, rows))
+
+        def delete_rows(txn):
+            txn.execute("DELETE FROM twoost_the_table")
+
+        yield db.runInteraction(delete_rows)
+        rows = yield db.runQuery("SELECT x FROM twoost_the_table")
+        self.assertEquals([], rows)
+
+        yield db.runOperation("DROP TABLE IF EXISTS twoost_the_table")
+        db.close()
